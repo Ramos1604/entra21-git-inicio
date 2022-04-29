76184823c0fcd274c878d6ed3422d01163875b65
Nathan César Ramos <ncramos@furb.br>
2022-04-29 20:47:13 -0300

    continuar

--- aula1/src/exercicio7.java
+++ aula1/src/exercicio7.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class exercicio7 {
     public static void main(String[] args) {
-        
+
+        double Custo;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Custo de Fábrica de um Carro");
+        Custo = in.nextDouble();
+
+        double var = (100 + 45);
+        double var2 = (100 + 28);
+        double vf = (100 / Custo * var2);
+        double vff = (100 / vf * var);
+
+        System.out.println("Valor" + vff);
+
+
     }
 }
