bacc8be330ddc9a178f7cca5eec9a329f3f723d5
Nathan César Ramos <ncramos@furb.br>
2022-06-09 21:34:25 -0300

    parente

---
+++ Livraria/src/Main.java
@@ -0,0 +1,32 @@
+import classes.guardados.Estante;
+import classes.itens.Item;
+import classes.itens.Livro;
+import com.sun.source.tree.EmptyStatementTree;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public String[] Estante = new String[5];
+
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("-MENU: Me informe seu Item-");
+        System.out.println("(1) - DVD");
+        System.out.println("(2) - Livro");
+    }
+
+
+    
+
+        Livro l = new Livro();
+        l.setTitulo("Harry Potter e a Autora Transfóbica");
+        l.avaliar();
+        l.avaliar();
+        l.avaliar();
+        System.out.println(l.getTotalRating());
+
+
+    }
+}
---
+++ Livraria/src/classes/guardados/Estante.java
@@ -0,0 +1,92 @@
+package classes.guardados;
+
+import classes.itens.Item;
+
+public class Estante {
+    private String[] adicionais = new String[5];
+
+    private int capMaxima;
+    private Item[] itens;
+
+    public void adicionarAdicional(String adicional) {
+        for (int i = 0; i < 10; i++) {
+            if(this.adicionais[i] == null) {
+                this.adicionais[i] = adicional;
+                break;
+            }
+        }
+    }
+
+    public String[] getAdicionais() {
+        return adicionais;
+    }
+
+    public void setAdicionais(String[] adicionais) {
+        this.adicionais = adicionais;
+    }
+
+
+    public Estante(int capMaxima) {
+        setCapMaxima(capMaxima);
+        setItens(new Item[capMaxima]);
+        //TODO
+    }
+
+    public boolean estanteCheia() {
+        return this.quantidadeItens() == this.getCapMaxima();
+    }
+
+    public int quantidadeItens() {
+        int contator = 0;
+        for (Item i : this.getItens()) {
+            if (i != null) {
+                contator ++;
+            }
+        }
+        return contator;
+    }
+
+    public Item buscarItem(String titulo) {
+        for (Item i : this.getItens()) {
+            if (i != null && i.getTitulo().toLowerCase().contains(titulo.toLowerCase())) {
+                return i;
+            }
+        }
+        return null;
+    }
+
+
+    public boolean adicionarItem(Item item) {
+        for (int i = 0; i < this.getItens().length; i++) {
+            if ( this.getItens()[i] == null ) {
+                this.getItens()[i] = item;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Item removerItem(int posicao) {
+        Item i = this.getItens()[posicao];
+        this.getItens()[posicao] = null;
+        return i;
+    }
+
+    // GETTERS & SETTERS
+
+    public int getCapMaxima() {
+        return capMaxima;
+    }
+
+    public void setCapMaxima(int capMaxima) {
+        this.capMaxima = capMaxima;
+    }
+
+    public Item[] getItens() {
+        return itens;
+    }
+
+    public void setItens(Item[] itens) {
+        this.itens = itens;
+    }
+}
